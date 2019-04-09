0260c03018bece74685d2c22497d91646b937719
Ivan <ivanchew92@gmail.com>
2019-04-09 16:05:21 +0800

    Included tracking codes 1

--- src/tracking_test.cpp
+++ src/tracking_test.cpp
@@ -3,14 +3,51 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/core/core.hpp>
+#include <opencv2/core/utility.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/aruco.hpp>
 #include <opencv2/tracking.hpp>
+#include <opencv2/tracking/tracker.hpp>
+#include <opencv2/tracking/tldDataset.hpp>
+#include <iostream>
+#include <cstring>
 
-cv::Ptr<cv::Tracker> tracker = cv::TrackerKCF::create();
 bool initialisation = true;
+std::string trackerType = "KCF";
+cv::Ptr<cv::MultiTracker> multiTracker = cv::MultiTracker::create();
+std::vector<std::string> trackerTypes;
+
+// create tracker by name
+cv::Ptr<cv::Tracker> createTrackerByName(std::string trackerType) 
+{
+  cv::Ptr<cv::Tracker> tracker;
+  if (trackerType ==  trackerTypes[0])
+    tracker = cv::TrackerBoosting::create();
+  else if (trackerType == trackerTypes[1])
+    tracker = cv::TrackerMIL::create();
+  else if (trackerType == trackerTypes[2])
+    tracker = cv::TrackerKCF::create();
+  else if (trackerType == trackerTypes[3])
+    tracker = cv::TrackerTLD::create();
+  else if (trackerType == trackerTypes[4])
+    tracker = cv::TrackerMedianFlow::create();
+  else if (trackerType == trackerTypes[5])
+    tracker = cv::TrackerGOTURN::create();
+  else if (trackerType == trackerTypes[6])
+    tracker = cv::TrackerMOSSE::create();
+  //~ else if (trackerType == trackerTypes[7])
+    //~ tracker = cv::TrackerCSRT::create();
+  else {
+    ROS_DEBUG_STREAM("Incorrect tracker name");
+    ROS_DEBUG_STREAM("Available trackers are: ");
+    for (std::vector<std::string>::iterator it = trackerTypes.begin() ; it != trackerTypes.end(); ++it)
+      ROS_DEBUG_STREAM(" " << *it);
+  }
+  return tracker;
+}
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
@@ -20,24 +57,28 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   cv::Mat image_grey;
   cv::Mat image_canny;
   cv::Mat image_drawing;
+  cv::Mat image_tracker;
   
   int thresh = 100;
   int max_thresh = 255;
   cv::RNG rng(12345);
   
+  cv::Scalar color_red = cv::Scalar(0,0,255);
+  cv::Scalar color_blue = cv::Scalar(0,255,0); //Color is BGR
+  
   try
   {
     image = cv_bridge::toCvShare(msg, "bgr8")->image;
     cv::cvtColor( image, image_grey, CV_BGR2GRAY);
+    cv::cvtColor( image, image_tracker, CV_BGR2GRAY);
     cv::blur( image_grey, image_grey, cv::Size(3,3) );
     cv::Canny( image_grey, image_canny, thresh, thresh*2, 3 );
-    cv::findContours( image_canny, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );
-    
+    cv::findContours( image_canny, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, cv::Point(0, 0) );    
     image_drawing = cv::Mat::zeros( image_canny.size(), CV_8UC3 );
     
     std::vector<std::vector<cv::Point> > contours_poly( contours.size() );
-    std::vector<cv::Rect> boundRect( contours.size() );
-    std::vector<cv::Point2f> center( contours.size() );
+    std::vector<cv::Rect2d> boundRect( contours.size() );
+    //~ std::vector<cv::Point2f> center( contours.size() );
     //~ std::vector<float> radius( contours.size() );
     
     for( int i = 0; i< contours.size(); i++ ){
@@ -47,20 +88,39 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
     
     for( int i = 0; i< contours.size(); i++ )
     {
-      //~ cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) ); //For random colors
-      cv::Scalar color_blue = cv::Scalar(0,255,0); //Color is BGR
-      cv::drawContours( image_drawing, contours, i, color_blue, 2, 8, hierarchy, 0, cv::Point() );
-      cv::Scalar color_red = cv::Scalar(0,0,255);
-      cv::rectangle( image_drawing, boundRect[i].tl(), boundRect[i].br(), color_red, 2, 8, 0);
-      
+      //~ cv::Scalar color = cv::Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) ); //For random colors      
+      cv::drawContours( image_drawing, contours, i, color_blue, 2, 8, hierarchy, 0, cv::Point() );      
+      cv::rectangle( image_drawing, boundRect[i].tl(), boundRect[i].br(), color_red, 2, 8, 0);      
     }
     
-    if (initialisation){ //Configure Region of Interest (ROI)
-      
+    if (initialisation){
+      initialisation = false;
+      ROS_DEBUG_STREAM("Initialisation Contour Size: " << contours.size());
+      for( int i = 0; i< contours.size(); i++ ){
+        multiTracker->add(createTrackerByName(trackerType), image_grey, boundRect[i]);
+      }
+    } else {
+      multiTracker->update(image_grey);
+      ROS_DEBUG_STREAM("Tracking Object Size: " << multiTracker->getObjects().size());
+      for(unsigned i=0; i<multiTracker->getObjects().size(); i++)
+      {
+        //~ cv::rectangle(image_tracker, multiTracker->getObjects()[i], color_red, 2, 1);
+        cv::Point rect_center = ((multiTracker->getObjects()[i].br() + multiTracker->getObjects()[i].tl())*0.5);
+        cv::circle(image_tracker, rect_center, 1, color_red, 1, 8, 0); 
+        ROS_DEBUG_STREAM("Object " << i << " Point: " << rect_center);
+      }
     }
-
+    
+    //Test
+    cv::Point image_center;
+    image_center.x = image_tracker.cols/2;
+    image_center.y = image_tracker.rows/2;
+    cv::circle(image_tracker, image_center, 1, color_red, 1, 8, 0); 
+    ROS_DEBUG_STREAM("Image Center " << image_center);
+    
     cv::imshow("view", image_drawing);
     cv::imshow("original", image);
+    cv::imshow("tracker", image_tracker);
     cv::waitKey(30);
   }
   catch (cv_bridge::Exception& e)
@@ -73,6 +133,8 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "image_listener");
   ros::NodeHandle nh;
+  ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug);
+  
   cv::namedWindow("view", cv::WINDOW_NORMAL);
   cv::moveWindow("view", 0,0);
   cv::resizeWindow("view", 1712,960);
@@ -80,10 +142,25 @@ int main(int argc, char **argv)
   cv::namedWindow("original", cv::WINDOW_NORMAL);
   cv::moveWindow("original", 2000,0);
   cv::resizeWindow("original", 1712,960);
+
+  cv::namedWindow("tracker", cv::WINDOW_NORMAL);
+  cv::moveWindow("tracker", 0,1100);
+  cv::resizeWindow("tracker", 1712,960);
   
   cv::startWindowThread();
+  
   image_transport::ImageTransport it(nh);
   image_transport::Subscriber sub = it.subscribe("/videofile/image_raw", 1, imageCallback);
+  
+  trackerTypes.push_back("BOOSTING");
+  trackerTypes.push_back("MIL");
+  trackerTypes.push_back("KCF");
+  trackerTypes.push_back("TLD");
+  trackerTypes.push_back("MEDIANFLOW");
+  trackerTypes.push_back("GOTURN");
+  trackerTypes.push_back("MOSSE");
+  trackerTypes.push_back("CSRT");
+
   ros::spin();
   cv::destroyWindow("view");
 }

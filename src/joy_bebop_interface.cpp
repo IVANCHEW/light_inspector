b9e0758835189d4cf6b6f9304a3a76a06083c346
IVANCHEW <ivanchew92@gmail.com>
2019-04-16 19:31:23 +0700

    Added autopilot command

--- src/joy_bebop_interface.cpp
+++ src/joy_bebop_interface.cpp
@@ -3,10 +3,12 @@
 
 #include <geometry_msgs/Twist.h>
 #include <std_msgs/Empty.h>
-
+#include "std_msgs/String.h"
+	
 ros::Publisher landing_pub;
 ros::Publisher takeoff_pub;
 ros::Publisher cmd_vel_pub;
+ros::Publisher auto_pilot_pub;
 std::string yaml_path_;
 std::string package_path_;
 std::string image_topic_ 	= "/bebop/image_raw";
@@ -14,8 +16,11 @@ std::string odom_topic_ 	= "/bebop/odom";
 std::string takeoff_topic_ 	= "/bebop/takeoff";
 std::string landing_topic_ 	= "/bebop/land";
 std::string cmd_vel_topic_ 	= "/bebop/cmd_vel";
+std::string auto_pilot_topic_ = "/autoflight/start";
+
 bool debug_ = true;
 geometry_msgs::Twist geo_msg;
+std_msgs::String auto_pilot_file_;
 std_msgs::Empty empty_msg;
 
 void take_off_command(){
@@ -37,54 +42,62 @@ void hover(){
 }
 
 void update_vel_command(){
-  ROS_DEBUG_STREAM("Update Velocity");
-  cmd_vel_pub.publish(geo_msg);
+	ROS_DEBUG_STREAM("Update Velocity");
+	cmd_vel_pub.publish(geo_msg);
 }
 
 void cmdVelCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
 	ROS_DEBUG_STREAM("Command Velocity Callback");
-  geo_msg.linear.x = msg->linear.x;
+	geo_msg.linear.x = msg->linear.x;
 	geo_msg.linear.y = msg->linear.y;
 	geo_msg.linear.z = msg->linear.z;
-  geo_msg.angular.z = msg->angular.z;
+	geo_msg.angular.z = msg->angular.z;
 }
 
 void takeOffCallback(const std_msgs::Empty::ConstPtr& received_msg)
 {
-  ROS_DEBUG_STREAM("Take Off Callback");
-  take_off_command();
+	ROS_DEBUG_STREAM("Take Off Callback");
+	take_off_command();
 }
 
 void landingCallback(const std_msgs::Empty::ConstPtr& received_msg)
 {
-  ROS_DEBUG_STREAM("Landing Callback");
-  land_command();
+	ROS_DEBUG_STREAM("Landing Callback");
+	land_command();
+}
+
+void autoPilotCallback(const std_msgs::String received_msg)
+{
+	ROS_DEBUG_STREAM("Autopilot Callback");
+	auto_pilot_file_.data = received_msg.data;
+	auto_pilot_pub.publish(auto_pilot_file_);
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "joy_bebop_interface");
-  ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug);
-  ROS_INFO("Node initialised");
-  
-  ros::NodeHandle nh_;
+	ros::init(argc, argv, "joy_bebop_interface");
+	ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug);
+	ROS_INFO("Node initialised");
+
+	ros::NodeHandle nh_;
 
 	landing_pub = nh_.advertise<std_msgs::Empty>(landing_topic_, 1);
 	takeoff_pub = nh_.advertise<std_msgs::Empty>(takeoff_topic_, 1);
 	cmd_vel_pub = nh_.advertise<geometry_msgs::Twist>(cmd_vel_topic_, 1);
-  
-  ros::Subscriber landing_pub = nh_.subscribe<std_msgs::Empty>("/bebop_joy/land", 10, landingCallback);
-  ros::Subscriber takeoff_sub_ = nh_.subscribe<std_msgs::Empty>("/bebop_joy/takeoff", 10, takeOffCallback);
-  ros::Subscriber cmd_update_sub_ = nh_.subscribe<geometry_msgs::Twist>("/bebop_joy/cmd_vel", 10, cmdVelCallback);
-  
-  ros::Rate loop_rate(20);
-  
-  while (ros::ok()){
-    
-    ros::spinOnce();
-    update_vel_command();
-    loop_rate.sleep();
-  }
+	auto_pilot_pub = nh_.advertise<std_msgs::String>(auto_pilot_topic_, 1);
+	
+	ros::Subscriber landing_pub = nh_.subscribe<std_msgs::Empty>("/bebop_joy/land", 10, landingCallback);
+	ros::Subscriber takeoff_sub_ = nh_.subscribe<std_msgs::Empty>("/bebop_joy/takeoff", 10, takeOffCallback);
+	ros::Subscriber cmd_update_sub_ = nh_.subscribe<geometry_msgs::Twist>("/bebop_joy/cmd_vel", 10, cmdVelCallback);
+	ros::Subscriber auto_pilot_sub_ = nh_.subscribe<std_msgs::String>("/bebop_joy/autopilot", 10, autoPilotCallback);
+
+	ros::Rate loop_rate(20);
+
+	while (ros::ok()){
+		ros::spinOnce();
+		update_vel_command();
+		loop_rate.sleep();
+	}
   
 }
--- src/turtle_teleop_joy.cpp
+++ src/turtle_teleop_joy.cpp
@@ -4,6 +4,10 @@
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
 #include <std_msgs/Empty.h>
+#include "std_msgs/Bool.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/Pose.h"
 
 class TeleopTurtle
 {
@@ -12,12 +16,17 @@ public:
 
 private:
 	void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
-
+	void LandingBoolCallback(const std_msgs::Bool bool_msg_);
+	void LandingTvecCallback(const geometry_msgs::PoseStamped pose_msg);
+	
 	ros::NodeHandle nh_;
 	std::string takeoff_topic_ 	= "/bebop_joy/takeoff";
 	std::string landing_topic_ 	= "/bebop_joy/land";
 	std::string cmd_vel_topic_ 	= "/bebop_joy/cmd_vel";
+	std::string auto_pilot_topic_ = "/bebop_joy/autopilot";
 	bool debug_ = true;
+	bool active_track_ = false;
+	bool track_marker_available_ = false;
 	int linear_, angular_;
 	double l_scale_, a_scale_;
 	float cmd_vel_increment_;
@@ -25,9 +34,13 @@ private:
 	ros::Publisher takeoff_pub;
 	ros::Publisher landing_pub;
 	ros::Publisher cmd_vel_pub;
+	ros::Publisher auto_pilot_pub;
 	geometry_msgs::Twist geo_msg;
 	std_msgs::Empty empty_msg;
+	std_msgs::String auto_pilot_file_;
 	ros::Subscriber joy_sub_;
+	ros::Subscriber landing_bool_sub_;
+	ros::Subscriber landing_tvec_sub_;
 	
 	void take_off_command(){
 		ROS_INFO("Initiate Takeoff");
@@ -37,6 +50,12 @@ private:
 	void land_command(){
 		ROS_INFO("Initiate Landing");
 		landing_pub.publish(empty_msg);
+	}
+	
+	void auto_pilot_command(){
+		ROS_INFO("Initiate Autopilot");
+		auto_pilot_file_.data = "autopilot.xml";
+		auto_pilot_pub.publish(auto_pilot_file_);
 	}
 	
 	void hover(){
@@ -50,6 +69,22 @@ private:
 	void update_vel_command(){
 		cmd_vel_pub.publish(geo_msg);
 	}
+	
+	void track_landing_command(){		
+		if(track_marker_available_){
+			ROS_INFO("Initiate Track Landing");
+			active_track_ = true;	
+		}	
+	}
+	
+	void cancel_track_landing(){
+		ROS_INFO("Cancel Track Landing");
+		active_track_ = false;
+	}
+	
+	void calculate_track_action(){
+		
+	}
 
 };
 
@@ -59,40 +94,77 @@ TeleopTurtle::TeleopTurtle()
 	landing_pub = nh_.advertise<std_msgs::Empty>(landing_topic_, 1);
 	takeoff_pub = nh_.advertise<std_msgs::Empty>(takeoff_topic_, 1);
 	cmd_vel_pub = nh_.advertise<geometry_msgs::Twist>(cmd_vel_topic_, 1);
+	auto_pilot_pub = nh_.advertise<std_msgs::String>(auto_pilot_topic_, 1);	
 	joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopTurtle::joyCallback, this);
+	landing_bool_sub_ = nh_.subscribe<std_msgs::Bool>("/landing/marker_found", 1, &TeleopTurtle::LandingBoolCallback, this);
+	landing_tvec_sub_ = nh_.subscribe<geometry_msgs::PoseStamped>("/landing/marker_pose", 1, &TeleopTurtle::LandingTvecCallback, this);
 }
 
 void TeleopTurtle::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
 	geometry_msgs::Twist twist;
-	geo_msg.linear.x = joy->axes[1];
-	geo_msg.linear.y = joy->axes[0];
-	//Second joy stick left-right
-	geo_msg.angular.z = joy->axes[2];
-	//Second joy stick up-down
-	geo_msg.linear.z = joy->axes[5];
+	
+	if (!active_track_){
+		geo_msg.linear.x = joy->axes[1];
+		geo_msg.linear.y = joy->axes[0];
+		geo_msg.linear.z = joy->axes[5];
+		geo_msg.angular.z = joy->axes[2];
+	} else {
+		calculate_track_action();
+	}
 	
 	ROS_DEBUG_STREAM(geo_msg);
 	
-	if (joy->buttons[2] == 1){
-	  //Right Palm Cirlce Button
-	  ROS_DEBUG_STREAM("Button 1 pressed");
-	  take_off_command();
-	} else if (joy->buttons[3] == 1){
-	  //Right Palm Triangle Button
-	  ROS_DEBUG_STREAM("Button 2 pressed");
-	  hover();
-	} else if (joy->buttons[3] == 1){
-	  //Right Palm Square Button
-	  ROS_DEBUG_STREAM("Button 3 pressed");
-	} else if (joy->buttons[1] == 1){
-	  //Right Palm Cross Button
-	  land_command();
-	  ROS_DEBUG_STREAM("Button 0 pressed");
+	if (joy->buttons[2] == 1){ //Circle
+		take_off_command();		
+	} else if (joy->buttons[0] == 1){ //Square
+		track_landing_command();
+	} else if (joy->buttons[3] == 1){ //Triangle
+		cancel_track_landing();
+	} else if (joy->buttons[1] == 1){ //Cross
+		land_command();
+	} else if (joy->buttons[4] == 1 && joy->buttons[5] == 1){ //R1 and L1
+		auto_pilot_command();
 	} 
 	update_vel_command();	
 }
 
+void TeleopTurtle::LandingBoolCallback(const std_msgs::Bool bool_msg_)
+{
+	track_marker_available_ = bool_msg_.data;
+}
+
+void TeleopTurtle::LandingTvecCallback(const geometry_msgs::PoseStamped pose_msg)
+{
+	if (track_marker_available_ && active_track_){
+		
+		//Forward movement
+		if (pose_msg.pose.position.z > 0.8){
+			geo_msg.linear.x = 0.2;
+		} else {
+			geo_msg.linear.x = 0;
+		}
+		
+		//Vertical movement
+		if (pose_msg.pose.position.y > 0.2){
+			geo_msg.linear.z = -0.2;
+		} else if (pose_msg.pose.position.y < -0.2) {
+			geo_msg.linear.z = 0.2;
+		} else {
+			geo_msg.linear.z = 0;
+		}
+		
+		//Sideways movement
+		if (pose_msg.pose.position.x > 0.2){
+			geo_msg.linear.y = -0.2;
+		} else if (pose_msg.pose.position.x < -0.2) {
+			geo_msg.linear.y = 0.2;
+		} else {
+			geo_msg.linear.y = 0;
+		}
+	}	
+	update_vel_command();
+}
 
 int main(int argc, char** argv)
 {
